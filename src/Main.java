a7487425a7147af27930849dc02e83aa0f9f515a
Yulia Ivanova <ivanova.yuvl@gmail.com>
2023-02-27 18:55:34 +0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int customerAccount = 200;
+        int depositAmount = 1000;
+        int amountOnAccount = (customerAccount + depositAmount);
+
+
+        int bonusRubles;
+        if (depositAmount >= 1000) {
+                    bonusRubles = (depositAmount / 100);
+                } else {
+            bonusRubles = 0;
+        }
+        int grandTotal = (customerAccount + depositAmount + bonusRubles);
+
+                    System.out.println("Счёт: " + grandTotal);
+                    System.out.println("Бонусные рубли: " + bonusRubles);
+
+    }
+}
